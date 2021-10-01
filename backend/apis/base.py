642ea8a5136f663ac5108436d33d395f00d93696
Abhishek Shimpi <88659222+abhishekshimpi31@users.noreply.github.com>
2021-10-01 13:35:48 +0530

    Updated

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -17,7 +17,7 @@ api_router.include_router(route_user.router, prefix="/user", tags=['users'])
 api_router.include_router(
     route_device.router, prefix="/device", tags=['devices'])
 api_router.include_router(route_group.router, prefix="/devicegroup", tags=['devicegroup'])
-api_router.include_router(route_association.router, prefix="/group-device-relation", tags=['group-device-relation'])
+api_router.include_router(route_association.router, prefix="/group-device", tags=['group-device-relation'])
 api_router.include_router(route_login.router, prefix="/login", tags=['/login'])
 api_router.include_router(route_role.router, prefix="/role", tags=['/roles'])
 api_router.include_router(route_userrole.router, prefix="/user_role", tags=['/userrole'])
