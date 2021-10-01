efd610118b6c9df98d1e4802410bb33951b483b2
Abhishek Shimpi <88659222+abhishekshimpi31@users.noreply.github.com>
2021-10-01 13:31:37 +0530

    Updated

--- backend/apis/version1/route_association.py
+++ backend/apis/version1/route_association.py
@@ -1,3 +1,7 @@
+from typing import List, Optional
+
+from fastapi.param_functions import Query
+from core.config import settings
 from db.repository.device import retrived_device
 from db.models.groupdevice import DeviceGroups
 from db.schemas.association import AssociationCreate
@@ -17,48 +21,53 @@ from db.repository.userrole import get_role
 router = APIRouter()
 
 
-@router.post("/create")
-def create_new_association(association:AssociationCreate, 
+@router.post("/association")
+def create_new_association(group_id:int, device_id: Optional[List[int]]=Query(None),
                             current_user: Users = Depends(get_current_user_from_token),
                             db: Session = Depends(get_db)):
     owner_id = current_user.user_id
     userrole = get_role(user_id=owner_id, db=db)
-    group_owner = retrived_group(group_name=association.group_name,db=db)
-    device_owner = retrived_device(device_hostname=association.device_hostname,db=db)
+    group_owner = retrived_group(group_id=group_id,db=db)
+    for n in device_id:
+        device_owner = retrived_device(device_id=n,db=db)
+        if not device_owner:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"{n} dose not exist")
     if not group_owner:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"{association.group_name} dose not exist")
-    if not device_owner:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"{association.device_hostname} dose not exist")
-    if userrole.role_id == 1 or userrole.role_id == 2: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"{group_id} dose not exist")
+    if userrole.role_id == settings.ADMIN or userrole.role_id == settings.MANAGER:
         if owner_id == group_owner.group_owner_id:
             try:
-                create_association(associate=association,db=db,owner_id=owner_id)
-                return "Successfully Created"
+                for n in device_id:
+                    creation = create_association(group_id=group_id,device_id=n,db=db,owner_id=owner_id)
+
+                return creation
             except:
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"{association.device_hostname} is aleardy exist in {association.group_name}")
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"{device_id} is aleardy exist in {group_id}")
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="You are not authorise to modify this group")
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="You have not privilages to make changes")
 
 
 
-@router.delete("/delete")
-def create_new_association(association:AssociationCreate, 
+@router.delete("/{group_id}/{device_id}")
+def delete_associations(group_id:int, device_id:int,
                             current_user: Users = Depends(get_current_user_from_token),
                             db: Session = Depends(get_db)):
     owner_id = current_user.user_id
     userrole = get_role(user_id=owner_id, db=db)
-    group_owner = retrived_group(group_name=association.group_name,db=db)
-    device_owner = retrived_device(device_hostname=association.device_hostname,db=db)
+    group_owner = retrived_group(group_id=group_id,db=db)
+    device_owner = retrived_device(device_id=device_id,db=db)
     if not group_owner:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"{association.group_name} dose not exist in groups")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"{group_id} dose not exist in groups")
     if not device_owner:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"{association.device_hostname} dose not exist in devices")
-    if userrole.role_id == 1 or userrole.role_id == 2: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"{device_id} dose not exist in devices")
+    if userrole.role_id == settings.ADMIN or userrole.role_id == settings.MANAGER:
         if owner_id == group_owner.group_owner_id:
             try:
-                delete_association(associate=association, db=db, owner_id=owner_id)
+                delete_association(group_id=group_id, device_id=device_id,  db=db, owner_id=owner_id)
                 return "Successfully Deleted"
             except:
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"{association.device_hostname} is aleardy exist in {association.group_name}")
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="You are not authorise to modify this group")
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"{device_id} is not exist in {group_id}")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="You are not authorise to modify this association")
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="You have not privilages to make changes")
+
+
